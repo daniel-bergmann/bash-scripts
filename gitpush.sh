887e36ca951f8a97fb2d3566032cf072a8db5ac5
Daniel Bergmann <danielbergmann@ok.is>
2023-06-21 14:15:38 +0000

    Update gitpush.sh

--- gitpush.sh
+++ gitpush.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Prompt the user for a commit message
+###
+# Make the script executable by running chmod +x git_push.sh.
+###
+
 read -p "Enter the commit message: " commit_message
-
-# Perform git add
 git add .
-
-# Perform git commit
 git commit -m "$commit_message"
-
-# Perform git push
 git push
+
+
